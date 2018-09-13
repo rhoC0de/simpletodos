f1b9c1ec8f7aa46d5f5b1dd62e90264e91ff74eb
rhoCode <oreoluwaakanni@outlook.com>
2018-09-13 15:46:51 +0000

    Replace update and remove with methods

--- imports/ui/task.js
+++ imports/ui/task.js
@@ -1,10 +1,10 @@
 
-
+import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 
  
 
-import { Tasks } from '../api/tasks.js';
+// import { Tasks } from '../api/tasks.js';
 
  
 
@@ -18,17 +18,19 @@ Template.task.events({
 
     // Set the checked property to the opposite of its current value
 
-    Tasks.update(this._id, {
+    Meteor.call('tasks.setChecked', this._id, !this.checked);
 
-      $set: { checked: ! this.checked },
+    // Tasks.update(this._id, {
 
-    });
+    //   $set: { checked: ! this.checked },
+
+    // });
 
   },
 
   'click .delete'() {
-
-    Tasks.remove(this._id);
+    Meteor.call('tasks.remove', this._id);
+    // Tasks.remove(this._id);
 
   },
 
